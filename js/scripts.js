dba0ee0cfe7614d89ec16ea0b744c1ca0e36072e
daniel <danielguijarroga@gmail.com>
2022-11-08 13:50:14 +0100

    menu +

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+const boxMenuElemnt = document.getElementById('box-menu');
+const menuBarElement = document.getElementById('menu-bar');
+
+boxMenuElemnt.addEventListener('click', ()=> {menuBarElement.classList.toggle('menu-bar--cruz')})
